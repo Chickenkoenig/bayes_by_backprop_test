e92fdf78a20655e3869cc51470fc62fbf242cd02
Chickenkoenig <lucas-julian-hartmann@web.de>
2024-05-28 02:40:40 +0200

    add seeds

--- src/config_parser/parser.py
+++ src/config_parser/parser.py
@@ -1,6 +1,16 @@
 import src.config_parser.data_parser as data_parser
 import src.config_parser.model_parser as model_parser
+import numpy as np
+import torch
+import random
+
 def parse_config(cfg):
+    initialize_seed(cfg)
     models = model_parser.get_models(cfg)
     data = data_parser.get_data(cfg)
     return data, models
+
+def initialize_seed(cfg):
+    torch.manual_seed(cfg.seed)
+    np.random.seed(cfg.seed)
+    random.seed(cfg.seed)
